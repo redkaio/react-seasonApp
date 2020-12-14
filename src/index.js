533746392e0f5511d8d21028b6de22d08d0ab80c
Kaio C <chrno.92@gmail.com>
2020-12-14 14:59:40 -0300

    app refatorado e implementacao da utilizacao do SeasonDisplay component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, long: null };
-
-
-
-    }
+    state = { lat: null, errMessage: ''};
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
@@ -29,7 +24,7 @@ class App extends React.Component {
         }
 
         if (!this.state.errMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         return (
