50158393ce9e296a0f704e0f5d2eb416714ea8ec
redkaio <chrno.92@gmail.com>
2021-01-11 12:47:25 -0300

    remocao das condicionais do metodo render()

--- src/index.js
+++ src/index.js
@@ -1,43 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
+  state = { lat: null, errMessage: "" };
 
-    state = { lat: null, errMessage: ''};
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) =>
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        }),
+      (err) => this.setState({ errMessage: err.message })
+    );
+  }
 
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude, long: position.coords.longitude }),
-            (err) => this.setState({ errMessage: err.message })
-        );
+  renderContent() {
+    if (!this.state.errMessage && !this.state.lat) {
+      return <Spinner />;
     }
 
-
-    render() {
-        if (!this.state.errMessage && !this.state.lat) {
-            return <Spinner />
-        }
-
-        if (this.state.errMessage && !this.state.lat) {
-            return <div>Error: {this.state.errMessage}</div>
-        }
-
-        if (!this.state.errMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-
-        return (
-            <div></div>
-
-        );
+    if (this.state.errMessage && !this.state.lat) {
+      return <div>Error: {this.state.errMessage}</div>;
     }
+
+    if (!this.state.errMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
 }
 
-ReactDOM.render(
-
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
